ec8098242e534c46853e5ba8f583a1e007c42116
tea551234 <tea551234@gmail.com>
2021-12-26 11:07:48 +0800

    12231107

--- JQ/20_取得元素+設定元素/55_get_input_value.js
+++ JQ/20_取得元素+設定元素/55_get_input_value.js
@@ -1,103 +1,50 @@
-// 10. 如果是這樣寫的話，有錯誤訊息，為什麼？
-btnGet.onclick = function(){
-    console.log("OK");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // 11. 畫面準備好以後才開始執行
 $(function () {
     btnGet.onclick = function () {
 
-        // 20. 取得 姓名欄位 (使用 JavaScript 的方式)
+        // // 20. 取得 姓名欄位 (使用 JavaScript 的方式)
+        // // 21. 取得 姓名欄位 (使用 jQuery 的方式)
+        // var js = document.getElementById('userName').value;
+        // var jq = $('#userName').prop('value');
+        // var jq2 = $('#userName').val();
+        // console.log(js);
+        // console.log(jq);
+        // console.log(jq2);
 
-        // 21. 取得 姓名欄位 (使用 jQuery 的方式)
 
-
-        // 30. 取得 地址欄位 (使用 JavaScript 的方式)
-
-        // 31. 取得 地址欄位 (使用 jQuery 的方式)
-
+        // // 30. 取得 地址欄位 (使用 JavaScript 的方式)
+        // // 31. 取得 地址欄位 (使用 jQuery 的方式)
+        // var js = document.getElementById('address').value;
+        // var jq = $('#address').prop('value');
+        // var jq2 = $('#address').val();
+        // console.log(js);
+        // console.log(jq);
+        // console.log(jq2);
 
         // 40. 取得 年紀欄位 (使用 JavaScript 的方式)
-
         // 41. 取得 年紀欄位 (使用 jQuery 的方式)
 
+        //     目標: 取得使用者輸入的年紀 
+        // 測試步驟: 等一下使用者選擇 20-30歲，點選按鈕，log 會出現 g2030
+
+        // 該怎麼達到目標????? 我要怎麼知道使用者選到哪一個????
+        // //  JavaScript
+        // //      1. 先取得 name="age" 這些元素 (目前有4個)
+        // //      2. 在步驟1找到的4個裡面被選擇的那一個 for 
+        // //      3. 判斷 當元素是否有被選擇 (checked==true) 
+        // //      4. 抓那個元素的 value 顯示到 log
+        // var apple = document.getElementsByName('age');
+        // for (var i = 0; i < apple.length; i++) {
+        //     if(apple[i].checked == true){
+        //         console.log(apple[i].value);
+        //     }
+        // }
+
+        // JavaScript v2
+        var js3 = document.querySelector('input[name="age"]:checked').value;
+        console.log(js3);
+
+        var jq3 = $('input[name="age"]:checked').val();
+        console.log(jq3);
     }
 })
-
-
-
--- JQ/20_取得元素+設定元素/56_set_input.js
+++ JQ/20_取得元素+設定元素/56_set_input.js
@@ -2,14 +2,16 @@ $(function () {
 
     // 10. 練習設定初始值
     btnSet.onclick = function () {
-
         // 20. 將地址預設為台中
-
+        // $('#address').val('Miaoli');
+        // $('#address').prop('value', 'Taichung');
+        // $('select option[value="Hsinchu"]').prop('selected', true)
 
         // 30. 將職業預設為其他
-
-
+        $('input[value="other"][name="job"]').prop('checked', true);
+        
         // 40. 將交通工具預設為機車
+        $('input[value="motorcycle"][name="vehicle"]').prop('checked', true);
 
     }
 })
