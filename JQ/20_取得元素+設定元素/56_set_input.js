b2c5cf3ea01c0a0102c500f3f0685126a7257318
tea551234 <tea551234@gmail.com>
2021-12-27 09:53:08 +0800

    12270953

--- JQ/20_取得元素+設定元素/56_set_input.js
+++ JQ/20_取得元素+設定元素/56_set_input.js
@@ -1,7 +1,8 @@
-$(function () {
+$(function() {
+
 
     // 10. 練習設定初始值
-    btnSet.onclick = function () {
+    btnSet.onclick = function() {
         // 20. 將地址預設為台中
         // $('#address').val('Miaoli');
         // $('#address').prop('value', 'Taichung');
@@ -9,10 +10,9 @@ $(function () {
 
         // 30. 將職業預設為其他
         $('input[value="other"][name="job"]').prop('checked', true);
-        
+
         // 40. 將交通工具預設為機車
         $('input[value="motorcycle"][name="vehicle"]').prop('checked', true);
 
     }
 })
-
