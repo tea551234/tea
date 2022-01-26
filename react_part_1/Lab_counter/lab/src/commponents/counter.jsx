b5ae9c8ed5637c322456e363f62b1402a1db5785
tea551234 <tea551234@gmail.com>
2022-01-26 15:07:53 +0800

    202201261507

--- react_part_1/Lab_counter/lab/src/commponents/counter.jsx
+++ react_part_1/Lab_counter/lab/src/commponents/counter.jsx
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
     }
-    styleList = {
-        fontSize : '100px',
-        fontWeight : 'bold'
+    styles = {
+        fontSize: '16px',
+        fontWeight: "bold"
     }
-
     render() {
-
         return (
             <div>
-                <span style={this.styleList}> {this.formatCount()} </span>
-                <button className='btnCounter'> 按我 </button>
+                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.okbtn} className='btn btn-outline-success btn-sm'>按我!!!!</button>
             </div>
-        )
+        );
     }
 
-formatCount(){
-    // return "ok"
-    const{ count } = this.state;
-    return count === 0 ? '0' : count;
+    okbtn = () => {
+        // this.state.count += 1;
+        // this.setState({})
+
+        // this.setState( { count: ++this.state.count  } )
+
+        let newState = { ...this.state }
+        newState.count += 1;
+        this.setState(newState)
+    }
+
+
+
+
+    getBadgeClasses() {
+        var classes = "badge m-2 badge-";
+        classes += this.state.count == 0 ? "warning" : "primary";
+        return classes;
+    }
+    formatCount() {
+        const { count } = this.state;
+        return (count == 0) ? "Zero" : count;
+    }
+
+
+    getTage() {
+        return this.state.tage.length === 0 ? "87" : "哭阿"
+    }
 }
 
 
-}
 
 
 export default Counter;
--- react_part_1/Lab_counter/lab/src/index.js
+++ react_part_1/Lab_counter/lab/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Couter from './commponents/counter.jsx'
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(
