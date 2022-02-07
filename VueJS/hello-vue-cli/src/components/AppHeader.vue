98af9a29bea3ee298d5705973b2a1796943a58a5
tea551234 <tea551234@gmail.com>
2022-02-07 16:23:52 +0800

    202202071623

---
+++ VueJS/hello-vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>{{ headerMessage }}</h1>
+    <hr>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+                headerMessage: "Hello! Vue.js"
+            }
+        }
+    }
+</script>
